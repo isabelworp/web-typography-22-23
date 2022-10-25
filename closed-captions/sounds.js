f188222d95e41f6958e750021e3034ac03da4972
isabelworp <94453248+isabelworp@users.noreply.github.com>
2022-10-25 23:18:48 +0200

    Add files via upload

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,23 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
+// sound is rij -5
 sounds = [
 	0,
 	9,
+	10,
 	12,
+	13,
 	15,
 	16.5,
+	17.5,
 	24,
 	25.8,
+	26.8,
 	31,
 	32,
-	34,
+	32.3,
+	33,
+	40,
+	75,	//16
 	86
 ];
